f31e74dbee73b04fc726614d9c363e9fce1b7b3b
Sultan Esbolatov <sultanesbolatov@gmail.com>
2024-12-24 16:49:16 +0500

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const goods = [
+    {
+        link: 'https://kaspi.kz/shop/p/apple-macbook-pro-14-2023-14-2-8-gb-ssd-512-gb-macos-mtl73-114877279/?c=750000000',
+        kaspiId:'114877279',
+        cityId: '750000000'
+    },
+    {
+        link: 'https://kaspi.kz/shop/p/apple-iphone-16-pro-max-256gb-zolotistyi-123890547/?c=750000000',
+        kaspiId:'123890547',
+        cityId: '750000000'
+    },
+    {
+        link: 'https://kaspi.kz/shop/p/apple-airpods-max-2-cherno-sinii-128622804/?c=750000000',
+        kaspiId:'128622804',
+        cityId: '750000000'
+    },
+]
